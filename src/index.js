e981ca88d5084994de4b8845cb12cd4262e9dc88
marcobrasci1986 <marco_brasci@hotmail.com>
2019-03-29 15:38:30 +0100

    JSX essentials

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+
+    const buttonText = {text: 'Click me'};
+    const style = {backgroundColor: 'blue', color: 'white'};
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <button style={style}>
+                {buttonText.text}
+            </button>
+        </div>
+    );
 };
 
 
