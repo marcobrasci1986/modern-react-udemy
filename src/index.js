ad60ef7d0e8c31b83b7916f1a3831b1b18c7b78a
marcobrasci1986 <marco_brasci@hotmail.com>
2019-03-29 15:11:31 +0100

    init setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Import the React and ReactDom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Create a react component
+const App = () => {
+    return <div>Hi there!</div>;
+};
+
+
+// Take the react component and shot it on the screen
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
